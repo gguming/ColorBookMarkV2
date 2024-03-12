30e3c018a39fcab515a377aba1aed1a8e3a07591
Jang HuiSeon <xkzl8521@gmail.com>
2024-03-12 23:04:09 +0900

    Feat: 프로젝트 초기화

---
+++ ColorBookMarkV2/ColorBookMarkV2/SignInViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignInViewController.swift
+//  ColorBookMarkV2
+//
+//  Created by SUN on 2/25/24.
+//
+
+import UIKit
+
+final class SignInViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        view.backgroundColor = .red
+    }
+}
